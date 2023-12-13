bacd0b020c299136b03f57268adbf7beb0f052af
Guilherme Simoes <gds.simoes@gmail.com>
2023-12-13 13:45:15 -0500

    Add component repeater utility function

--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,19 @@
 export const sample = (arr) => {
-  return arr[Math.floor(Math.random() * arr.length)];
+    return arr[Math.floor(Math.random() * arr.length)];
 };
 
 export const range = (start, end, step = 1) => {
-  let output = [];
-  if (typeof end === 'undefined') {
-    end = start;
-    start = 0;
-  }
-  for (let i = start; i < end; i += step) {
-    output.push(i);
-  }
-  return output;
+    let output = [];
+    if (typeof end === "undefined") {
+        end = start;
+        start = 0;
+    }
+    for (let i = start; i < end; i += step) {
+        output.push(i);
+    }
+    return output;
+};
+
+export const componentRepeater = (n, Component) => {
+    return range(n).map((_, index) => <Component key={index} />);
 };
